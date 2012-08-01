b1ec0d08664c64bd768ab571177cf9eebc6cdadf
TJ Holowaychuk <tj@vision-media.ca>
2012-08-01 15:46:45 -0700

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+
+# Path-to-RegExp
+
+  Turn an Express-style path string such as `/user/:name` into a regular expression.
+
+## License
+
+  MIT
---
+++ index.js
@@ -0,0 +1,51 @@
+
+/**
+ * Expose `pathtoRegexp`.
+ */
+
+module.exports = pathtoRegexp;
+
+/**
+ * Normalize the given path string,
+ * returning a regular expression.
+ *
+ * An empty array should be passed,
+ * which will contain the placeholder
+ * key names. For example "/user/:id" will
+ * then contain ["id"].
+ *
+ * @param  {String|RegExp|Array} path
+ * @param  {Array} keys
+ * @param  {Object} options
+ * @return {RegExp}
+ * @api private
+ */
+
+function pathtoRegexp(path, keys, options) {
+  options = options || {};
+  var sensitive = options.sensitive;
+  var strict = options.strictl
+
+  if (path instanceof RegExp) return path;
+  if (path instanceof Array) path = '(' + path.join('|') + ')';
+
+  path = path
+    .concat(strict ? '' : '/?')
+    .replace(/\/\(/g, '(?:/')
+    .replace(/\+/g, '__plus__')
+    .replace(/(\/)?(\.)?:(\w+)(?:(\(.*?\)))?(\?)?/g, function(_, slash, format, key, capture, optional){
+      keys.push({ name: key, optional: !! optional });
+      slash = slash || '';
+      return ''
+        + (optional ? '' : slash)
+        + '(?:'
+        + (optional ? slash : '')
+        + (format || '') + (capture || (format && '([^/.]+?)' || '([^/]+?)')) + ')'
+        + (optional || '');
+    })
+    .replace(/([\/.])/g, '\\$1')
+    .replace(/__plus__/g, '(.+)')
+    .replace(/\*/g, '(.*)');
+
+  return new RegExp('^' + path + '$', sensitive ? '' : 'i');
+};
