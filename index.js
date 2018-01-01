fd44ef8408e9cc73c4de605bcf11c42254eb601c
Alex Burlacu <alexburlacu96@gmail.com>
2018-01-01 22:57:51 +0100

    add comments from master

--- index.js
+++ index.js
@@ -13,9 +13,17 @@ const DEFAULT_DELIMITERS = './'
  *
  * @type {RegExp}
  */
-const PATH_REGEXP = new RegExp(
-  '(\\\\.)|(?:\\:(\\w+)(?:\\(((?:\\\\.|[^\\\\()])+)\\))?|\\(((?:\\\\.|[^\\\\()])+)\\))([+*?])?',
-  'g')
+const PATH_REGEXP = new RegExp([
+  // Match escaped characters that would otherwise appear in future matches.
+  // This allows the user to escape special characters that won't transform.
+  '(\\\\.)',
+  // Match Express-style parameters and un-named parameters with a prefix
+  // and optional suffixes. Matches appear as:
+  //
+  // "/:test(\\d+)?" => ["/", "test", "\d+", undefined, "?"]
+  // "/route(\\d+)"  => [undefined, undefined, undefined, "\d+", undefined]
+  '(?:\\:(\\w+)(?:\\(((?:\\\\.|[^\\\\()])+)\\))?|\\(((?:\\\\.|[^\\\\()])+)\\))([+*?])?'
+].join('|'), 'g')
 
 /**
  * Parse a string for the raw tokens.
