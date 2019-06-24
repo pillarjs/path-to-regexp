a913bb8fadbe2a0654735c44cdc803b462df5faf
Luiz Américo <luizamericop@gmail.com>
2019-06-24 17:14:42 -0300

    Add named exports to esm build

--- tools/create-esm.js
+++ tools/create-esm.js
@@ -4,6 +4,6 @@ const exportRegex = /module.exports\s*=\s*pathToRegexp/
 
 const cjsSource = fs.readFileSync('index.js', { encoding: 'utf8' })
 
-const esmSource = cjsSource.replace(exportRegex, 'export default pathToRegexp')
+const esmSource = cjsSource.replace(exportRegex, 'export default pathToRegexp\nexport { parse, compile, tokensToFunction, tokensToRegExp }\n')
 
 fs.writeFileSync('index.mjs', esmSource)
