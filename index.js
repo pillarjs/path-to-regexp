dbe3e183a43c0df33df9f063c787c82c65b74156
ctcpip <ctcpip@users.noreply.github.com>
2024-08-07 22:43:59 -0500

    ✨ change lookahead to non-capturing group

--- index.js
+++ index.js
@@ -123,7 +123,7 @@ function pathtoRegexp(path, keys, options) {
   }
 
   // If the path is non-ending, match until the end or a slash.
-  path += (end ? '$' : (path[path.length - 1] === '/' ? '' : '(?=\\/|$)'));
+  path += (end ? '$' : (path[path.length - 1] === '/' ? '' : '(?:\/|$)'));
 
   return new RegExp(path, flags);
 };
