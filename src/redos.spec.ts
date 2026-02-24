05d0a2e4d32f29b241dd9c3c65282b6bbe906705
Blake Embrey <hello@blakeembrey.com>
2026-02-24 09:18:44 -0800

    Restrict repeated wildcard backtracking

---
+++ src/redos.spec.ts
@@ -0,0 +1,14 @@
+import { check } from "recheck";
+import { pathToRegexp } from "./index.js";
+import { MATCH_TESTS } from "./cases.spec.js";
+import { describe, expect, it } from "vitest";
+
+describe.concurrent("redos", () => {
+  it.each(MATCH_TESTS.map((x) => [x.path, pathToRegexp(x.path).regexp]))(
+    "%s - %s",
+    async (_, regexp) => {
+      const result = await check(regexp.source, regexp.flags);
+      expect(result.status).toBe("safe");
+    },
+  );
+});
